6550ffc378bb6bd207c6348c6e370c162c01288d
Martyn <zogacc@gmail.com>
2022-12-17 20:21:27 +0400

    change name of env variable

--- src/constants/dev.js
+++ src/constants/dev.js
@@ -1,9 +1,7 @@
 import { TELEGRAM_BOT_URL } from "./bot.js"
 
-export const NGROK_URL = process.env.NGROK_URL
-
-export const WEB_APP_URL = process.env.RAILWAY_URL
+export const WEB_APP_URL = process.env.NGROK_URL // process.env.BOT_URL
 // export const WEB_APP_URL =
-//   process.env.NODE_ENV === "development" ? NGROK_URL : RENDER_URL
+//   process.env.NODE_ENV === "development" ? NGROK_URL : BOT_URL
 
 export const WEBHOOK_URL = TELEGRAM_BOT_URL + "/setWebhook?url=" + WEB_APP_URL
